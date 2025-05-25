14a03178fcd19110e5869fd3d69e278a8161ab9d
pegi4 <vito.senic@gmail.com>
2025-05-25 15:56:45 +0200

    KAN-42 Network module for connecting in emulator

---
+++ app/src/main/java/com/jvn/myapplication/data/api/NetworkModule.kt
@@ -0,0 +1,28 @@
+package com.jvn.myapplication.data.api
+
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+object NetworkModule {
+    private const val BASE_URL = "http://10.0.2.2:3000/" // Use this for Android emulator
+    // For physical device, use your computer's IP: "http://192.168.1.XXX:3000/"
+
+    private val loggingInterceptor = HttpLoggingInterceptor().apply {
+        level = HttpLoggingInterceptor.Level.BODY
+    }
+
+    private val okHttpClient = OkHttpClient.Builder()
+        .addInterceptor(loggingInterceptor)
+        .build()
+
+    val authApi: AuthApiService by lazy {
+        Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .client(okHttpClient)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+            .create(AuthApiService::class.java)
+    }
+}
