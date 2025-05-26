d0ece71b92fb0ca6aae436190b977fe1f87b9eb1
NikolaM4 <nikolamitrovic434@gmail.com>
2025-05-26 21:41:16 +0200

    KAN-39 Updated Manifest and Gradle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -58,13 +58,29 @@ dependencies {
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
     implementation("com.squareup.okhttp3:okhttp:4.11.0")
+    implementation("com.squareup.okhttp3:logging-interceptor:4.12.0")
+
     implementation("com.google.mlkit:barcode-scanning:17.2.0")
+
+    implementation ("androidx.camera:camera-core:1.3.0")
+    implementation ("androidx.camera:camera-video:1.3.0")
+    implementation ("androidx.camera:camera-extensions:1.3.0")
+
     implementation("androidx.camera:camera-camera2:1.3.0")
     implementation("androidx.camera:camera-lifecycle:1.3.0")
     implementation("androidx.camera:camera-view:1.3.0")
+
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
-    implementation("com.squareup.okhttp3:logging-interceptor:4.12.0")
+
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.7.0")
+
     implementation("androidx.datastore:datastore-preferences:1.0.0")
+
+    implementation ("com.google.mlkit:face-detection:16.1.5")
+
+    implementation ("androidx.exifinterface:exifinterface:1.3.6")
+
+    implementation ("androidx.activity:activity-compose:1.8.2")
+
 }
