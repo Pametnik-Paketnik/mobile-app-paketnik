2f1f9c1a95e67bca3aaff423dc736020511c51b1
NikolaM4 <nikolamitrovic434@gmail.com>
2025-06-02 23:58:46 +0200

    KAN-93 Bottom navigation items

---
+++ app/src/main/java/com/jvn/myapplication/ui/navigation/BottomNavItem.kt
@@ -0,0 +1,37 @@
+package com.jvn.myapplication.ui.navigation
+
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.History
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Settings
+import androidx.compose.ui.graphics.vector.ImageVector
+
+sealed class BottomNavItem(
+    val route: String,
+    val title: String,
+    val icon: ImageVector
+) {
+    object Home : BottomNavItem(
+        route = "home",
+        title = "Home",
+        icon = Icons.Default.Home
+    )
+    
+    object BoxesHistory : BottomNavItem(
+        route = "boxes_history",
+        title = "History",
+        icon = Icons.Default.History
+    )
+    
+    object UserSettings : BottomNavItem(
+        route = "user_settings",
+        title = "Settings",
+        icon = Icons.Default.Settings
+    )
+}
+
+val bottomNavItems = listOf(
+    BottomNavItem.Home,
+    BottomNavItem.BoxesHistory,
+    BottomNavItem.UserSettings
+) 
