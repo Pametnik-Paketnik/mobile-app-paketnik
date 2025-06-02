549fcfbe00fc878dc0b077e9f1ccb5399a38487d
pegi4 <vito.senic@gmail.com>
2025-06-03 00:04:42 +0200

    KAN-93 Main navigation that connects everything in nav

---
+++ app/src/main/java/com/jvn/myapplication/ui/navigation/MainNavigation.kt
@@ -0,0 +1,50 @@
+package com.jvn.myapplication.ui.navigation
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.jvn.myapplication.ui.screens.BoxesHistoryScreen
+import com.jvn.myapplication.ui.screens.HomeScreen
+import com.jvn.myapplication.ui.screens.UserSettingsScreen
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun MainNavigation(
+    onLogout: () -> Unit = {}
+) {
+    val navController = rememberNavController()
+    val softGray = Color(0xFFF8F9FA)
+
+    Scaffold(
+        modifier = Modifier.fillMaxSize(),
+        containerColor = softGray,
+        bottomBar = {
+            BottomNavigationBar(navController = navController)
+        }
+    ) { paddingValues ->
+        NavHost(
+            navController = navController,
+            startDestination = BottomNavItem.Home.route,
+            modifier = Modifier.padding(paddingValues)
+        ) {
+            composable(BottomNavItem.Home.route) {
+                HomeScreen()
+            }
+            
+            composable(BottomNavItem.BoxesHistory.route) {
+                BoxesHistoryScreen()
+            }
+            
+            composable(BottomNavItem.UserSettings.route) {
+                UserSettingsScreen(onLogout = onLogout)
+            }
+        }
+    }
+} 
